c13449807f948cbac8448c10157072f2a822a867
Steven Hall <Hallzy.18@gmail.com>
2021-11-30 22:38:11 -0800

    2021 Begins

---
+++ 2021/01/main.hs
@@ -0,0 +1,23 @@
+-- Part 1
+solve1 :: [Int] -> Int
+solve1 input = countIncrease 0 input
+    where
+        countIncrease count (x:y:ns)
+            | y > x = countIncrease (count + 1) (y : ns)
+            | otherwise = countIncrease count (y : ns)
+        countIncrease count _ = count
+
+-- Part 2
+solve2 :: [Int] -> Int
+solve2 input = countIncrease 0 input
+    where
+        countIncrease count (w:x:y:z:ns)
+            | (w + x + y) < (x + y + z) = countIncrease (count + 1) (x : y : z : ns)
+            | otherwise = countIncrease count (x : y : z : ns)
+        countIncrease count _ = count
+
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    putStrLn $ show $ solve2 $ map read $ lines contents
