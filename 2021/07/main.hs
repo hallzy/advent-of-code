74edf6e70ac4b2f7bf29403864f828fe582cc7fc
Steven Hall <Hallzy.18@gmail.com>
2021-12-06 22:50:19 -0800

    Improvement to Day 7

--- 2021/07/main.hs
+++ 2021/07/main.hs
@@ -7,11 +7,17 @@ split delim str = map read $ lines $ map aux str
             | item == delim = '\n'
             | otherwise = item
 
+-- Triangle number of n is n + (n - 1) + (n - 2) + ... + 1
+-- Yes, I could do a sum of [1..n], but that is significantly slower for this
+-- use case
+triangleNumber :: Int -> Int
+triangleNumber n = n*(n + 1) `div` 2
+
 calculateFuel1 :: Int -> [(Int, Int)] -> Int
-calculateFuel1 idx groupedList = foldl (\acc x -> abs(idx - (fst x)) * (snd x) + acc) 0 $ groupedList
+calculateFuel1 idx groupedList = foldl' (\acc (pos, count) -> acc + count * abs(idx - pos)) 0 groupedList
 
 calculateFuel2 :: Int -> [(Int, Int)] -> Int
-calculateFuel2 idx groupedList = foldl (\acc x -> (sum [1..abs(idx - (fst x))]) * (snd x) + acc) 0 $ groupedList
+calculateFuel2 idx groupedList = foldl' (\acc (pos, count) -> acc + count * (triangleNumber $ abs(idx - pos))) 0 groupedList
 
 solve :: Int -> [Int] -> String
 solve part list = show $ minimum $ aux min
@@ -35,4 +41,4 @@ main = do
     contents <- readFile "input.txt"
     let input = split ',' contents
     putStrLn $ "Part 1: " ++ (solve 1 input)
-    putStrLn $ "Part 1: " ++ (solve 2 input)
+    putStrLn $ "Part 2: " ++ (solve 2 input)
