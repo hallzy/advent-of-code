e5fb2917666e38be7471b08d5d3c5cb20022361c
Steven Hall <steven.hall@sendtonews.com>
2021-12-15 17:06:03 -0800

    Day 15

--- 2021/15/main.hs
+++ 2021/15/main.hs
@@ -8,46 +8,41 @@ type Graph          = [[Distance]]
 type PreviousVertex = Vertex
 type Distance       = Int
 type Visited        = Bool
-type Dijkstra       = [(Vertex, PreviousVertex, Distance, Visited)]
+type DijkstraVal    = (PreviousVertex, Distance, Visited)
+type DijkstraEl     = (Vertex, DijkstraVal)
+type Dijkstra       = Map.Map Vertex DijkstraVal
 
--- TODO: Dijkstra might be better as a Map on the Vertex (vertex can be removed
--- from the map value then)
+type UnvisitedList  = [(Distance, Vertex)]
 
--- TODO: Graph could be a map too. Maybe leave it for now unless I end up with
--- performance issues
+getAdjacencyList :: Vertex -> [Vertex]
+getAdjacencyList vertex = zipWith (\matrix vertex -> (fst matrix + fst vertex, snd matrix + snd vertex)) [(-1, 0), (1, 0), (0, -1), (0, 1)] $ replicate 4 vertex
 
--- Dijkstra
--- Keep Track of the nodes you have visited, and the nodes you haven't visited
--- Set the distance to your starting node as 0, because you are already there
--- The distance to all other nodes is inf until we evaluate them
+dijkstraElAsVertex :: DijkstraEl -> Vertex
+dijkstraElAsVertex el = fst el
 
--- Set distance from start to start as 0
--- Previous vertex is null
+getDistance :: DijkstraEl -> Int
+getDistance (_, (_, distance, _)) = distance
 
--- Visit the unvisited vertex with the lowest distance (initially, this is just
--- the start vertex)
+visitVertex :: DijkstraEl -> DijkstraEl
+visitVertex (v, (pv, d, _)) = (v, (pv, d, True))
 
--- Loop starts here
+insertUnvisited :: (Distance, Vertex) -> UnvisitedList -> UnvisitedList
+insertUnvisited newVertex currentVertex = aux False False newVertex currentVertex
+  where
+    aux hasInserted _ newVertex []
+      | hasInserted = []
+      | otherwise = [newVertex]
+    aux hasInserted hasRemoved newVertex@(newDistance, newCoord) (currentVertex@(currentDistance, currentCoord):ys)
+      | hasRemoved && hasInserted = ys
+      | (not hasInserted) && newDistance <= currentDistance = newVertex : currentVertex : aux True hasRemoved newVertex ys
+      | newCoord == currentCoord = aux hasInserted True newVertex ys
+      | otherwise = currentVertex : (aux hasInserted hasRemoved newVertex ys)
 
--- Examine the current vertex's unvisited neighbours
-
--- Calculate the distance to those neighbours from the current vertex. The
--- distance is the distance from the current vertex to the neighbour, plus the
--- distance assigned to the current vertex.
-
--- If the calculated distance is lower than the current calculated distance for
--- that vertex, then replace the distance, and set the current vertex as the
--- "previous vertex" for that neighbour.
-
--- Add the current vertex to the list of unvisited vertices and remove it from
--- the unvisited list
-
--- repeat
-
--- Algorithm is done when there are no more unvisited vertices
+dijkstraElToUnlisted :: DijkstraEl -> (Distance, Vertex)
+dijkstraElToUnlisted (vertex, (_, distance, _)) = (distance, vertex)
 
 dijkstra :: Coord -> Graph -> Dijkstra
-dijkstra start graph = aux [(start, start, 0, False)]
+dijkstra start graph = aux [(0, start)] $ Map.fromList [(start, (start, 0, False))]
   where
     rowCount = length graph
     colCount = length (head graph)
@@ -55,20 +50,89 @@ dijkstra start graph = aux [(start, start, 0, False)]
 
     startingUnvisited = range ((0, 0), (rowCount - 1, colCount - 1))
 
-    aux :: Dijkstra -> Dijkstra
-    aux dijkstra
+    aux :: UnvisitedList -> Dijkstra -> Dijkstra
+    aux unvisitedList dijkstra
       | length dijkstra == totalVertices = dijkstra
-      | otherwise = aux newDijkstra
+      | otherwise = aux newUnvisitedList newDijkstra
       where
-        nextVertex = foldl1' (\acc@(_, _, accDistance, _) new@(_, _, newDistance, _) -> if accDistance <= newDistance then acc else new) $ filter (\(_, _, _, visited) -> not visited) dijkstra
-        neighbours = undefined -- Get the neighbours of nextVertex with the updated distance
-        newDijkstra = undefined -- Add the neighbours to newDijkstra and update the distance and previous vertex
+        nextVertex :: DijkstraEl
+        nextVertex = (vertex, dijkstra Map.! vertex)
+          where
+            vertex = snd $ head unvisitedList
 
-solve1 :: Graph -> String
-solve1 graph = show $ dijkstra (0, 0) graph
+        newUnvisitedList :: UnvisitedList
+        newUnvisitedList = foldl' (flip insertUnvisited) (tail unvisitedList) $ filter (\(_, coord) -> Map.notMember coord dijkstra) $ map dijkstraElToUnlisted neighbours
+
+        visitedVertex :: DijkstraEl
+        visitedVertex = visitVertex nextVertex
+
+        isMember neighbour = Map.member neighbour dijkstra
+
+        getNeighbours :: [Vertex] -> [DijkstraEl]
+        getNeighbours [] = []
+        getNeighbours (v:vs)
+          | validIndex = (v, (dijkstraElAsVertex nextVertex, (getDistance nextVertex) + currentNeighbourWeight, False)) : (getNeighbours vs)
+          | otherwise = getNeighbours vs
+          where
+            row = fst v
+            col = snd v
+            validIndex = row >= 0 && row < rowCount && col >= 0 && col < colCount
+            currentNeighbourWeight = graph !! row !! col
+
+        neighbours = getNeighbours $ getAdjacencyList $ dijkstraElAsVertex nextVertex
+
+        newDijkstra :: Dijkstra
+        newDijkstra = Map.insert (fst visitedVertex) (snd visitedVertex) $ foldl' (\acc neighbour -> insert neighbour acc) dijkstra neighbours
+          where
+          insert :: DijkstraEl -> Dijkstra -> Dijkstra
+          insert (v, (pv, d, visited)) dijkstra
+            | isMember v = Map.insertWith (\acc@(accPv, accD, accVisited) (newPv, newD, newVisited) -> if accD <= newD then acc else (newPv, newD, accVisited || newVisited)) v (pv, d, visited) dijkstra
+            | otherwise = Map.insert v (pv, d, visited) dijkstra
+
+findDistance :: DijkstraVal -> Int
+findDistance (_, d, _) = d
+
+solve :: Graph -> String
+solve graph = show $ findDistance (dijkstraResults Map.! end)
+  where
+    rowCount = length graph
+    colCount = length (head graph)
+
+    dijkstraResults :: Dijkstra
+    dijkstraResults = dijkstra (0, 0) graph
+
+    end = (rowCount - 1, colCount - 1)
+
+printBoard :: Graph -> String
+printBoard = unlines . map (map (head . show))
+
+extendBoardDown :: Int -> Graph -> Graph
+extendBoardDown rowsPer board = board ++ (map (map inc) (drop ((length board)- rowsPer) board))
+  where
+    inc :: Int -> Int
+    inc val
+      | val == 9 = 1
+      | otherwise = val + 1
+
+extendBoardRight :: Int -> Graph -> Graph
+extendBoardRight colsPer board = map (\row -> row ++ (map inc (drop ((length row) - colsPer) row))) board
+  where
+    inc :: Int -> Int
+    inc val
+      | val == 9 = 1
+      | otherwise = val + 1
+
+biggerBoard :: Graph -> Graph
+biggerBoard board = aux 5 board
+  where
+    aux :: Int -> Graph -> Graph
+    aux multiplier auxBoard
+      | multiplier <= 1 = auxBoard
+      | otherwise = aux (multiplier - 1) $ (extendBoardDown (length board)) $ (extendBoardRight (length (head board))) auxBoard
 
 main :: IO ()
 main = do
    contents <- readFile "input.txt"
    let input =  map (map ((read :: String -> Int) . (:[]))) $ lines contents
-   putStrLn $ "Part 1: " ++ (solve1 input)
+   putStrLn $ "Part 1: " ++ (solve input)
+   putStrLn $ "Part 2: " ++ (solve (biggerBoard input))
